50ebf449de5f932eea0c003579398d495d4b98ce
Samet Aydinhan <samet.aydinhan879@gmail.com>
2025-06-02 13:35:04 +0300

    refactor: simplify Provider component and standardize imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
+import { Suspense } from "react";
 import { OnboardingForm } from "../onboarding-form";
 
 export default function Home() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <OnboardingForm />
-    </>
+    </Suspense>
   );
 }
--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,8 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ColorModeProvider } from "./color-mode";
+import type { ThemeProviderProps } from "next-themes";
 
-export function Provider(props: ColorModeProviderProps) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  )
+export function Provider(props: ThemeProviderProps) {
+  return <ColorModeProvider {...props} />;
 }
