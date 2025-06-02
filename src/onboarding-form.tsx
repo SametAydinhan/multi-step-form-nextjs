3c72c5c656ed88941f3b1de9a201c533d4c7995f
Samet Aydinhan <samet.aydinhan879@gmail.com>
2025-06-02 12:16:13 +0300

    feat: refactor onboarding form to use search parameters for step navigation

--- src/onboarding-form.tsx
+++ src/onboarding-form.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Box, Button, ButtonGroup, Heading, Steps } from "@chakra-ui/react";
+import { Box,  Heading, Steps } from "@chakra-ui/react";
 import PersonelInfo from "./personel-info";
 import WorkInfo from "./work-info";
 import MoreDetails from "./more-details";
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const steps = [
   { title: "Personal Information", id: "personal-info" },
@@ -13,18 +13,39 @@ const steps = [
 ];
 
 export function OnboardingForm() {
-  const [step, setStep] = useState(0);
+  const searchParams = useSearchParams();
+  const stepId = searchParams.get("step");
 
-    const handleNext = () => {
-        if (step < steps.length - 1) {
-        setStep(step + 1);
-        }
-    };
+  let step = steps.findIndex((s) => s.id === stepId);
+  if (step === -1) {
+    step = 0; // Default to the first step if not found
+  }
+  if(stepId === 'done') step = steps.length; // If 'done' is specified, go to the last step
+
+  const router = useRouter();
+
+
+  const handleNext = () => {
+    const nextStep = steps[step + 1];
+    router.push(`?step=${nextStep?.id ?? 'done'}`);
+  };
+
+  const goToStep = (step: number) => {
+    const nextStep = steps[step];
+    if (nextStep) {
+        router.push(`?step=${nextStep.id}`);
+    } 
+  }
+
 
   return (
     <Box padding={5} maxWidth='560px' mx='auto'>
       <Heading mb={4}>Onboarding Form</Heading>
-      <Steps.Root step={step} onStepChange={(e) => setStep(e.step)} count={steps.length}>
+      <Steps.Root
+        step={step}
+        onStepChange={(e) => goToStep(e.step)}
+        count={steps.length}
+      >
         <Steps.List>
           {steps.map((step, index) => (
             <Steps.Item key={index} index={index} title={step.title}>
@@ -36,27 +57,35 @@ export function OnboardingForm() {
         </Steps.List>
 
         <Steps.Content index={0}>
-          <PersonelInfo onSubmit={(data) => {
-            handleNext();
-            console.log("Personal Info Submitted:", data);
-          }}/>
+          <PersonelInfo
+            onSubmit={(data) => {
+              handleNext();
+              console.log("Personal Info Submitted:", data);
+            }}
+          />
         </Steps.Content>
 
         <Steps.Content index={1}>
-          <WorkInfo onSubmit={(data) => {
-            handleNext();
-            console.log("Work Info Submitted:", data);
-          }}/>
+          <WorkInfo
+            onSubmit={(data) => {
+              handleNext();
+              console.log("Work Info Submitted:", data);
+            }}
+          />
         </Steps.Content>
 
         <Steps.Content index={2}>
-          <MoreDetails onSubmit={(data) => {
-            handleNext();
-            console.log("More Details Submitted:", data);
-          }}/>
+          <MoreDetails
+            onSubmit={(data) => {
+              handleNext();
+              console.log("More Details Submitted:", data);
+            }}
+          />
         </Steps.Content>
 
-        <Steps.CompletedContent>Thank you for completing the onboarding process!</Steps.CompletedContent>
+        <Steps.CompletedContent>
+          Thank you for completing the onboarding process!
+        </Steps.CompletedContent>
       </Steps.Root>
     </Box>
   );
